732af6c4c0bd62da66ad630b655f2d9ceab542e8
shakao-test <61802990+shakao-test@users.noreply.github.com>
2021-03-23 09:43:48 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 namespace SpriteKind {
     export const Camera = SpriteKind.create()
 }
-let mySprite = sprites.create(image.create(150, 110), SpriteKind.Camera)
-mySprite.image.fill(4)
-controller.moveSprite(mySprite)
-mySprite.setFlag(SpriteFlag.Invisible, true)
-mySprite.setFlag(SpriteFlag.Ghost, true)
-mySprite.setFlag(SpriteFlag.StayInScreen, true)
-scene.cameraFollowSprite(mySprite)
+let __camera = sprites.create(image.create(150, 110), SpriteKind.Camera)
+__camera.image.fill(4)
+controller.moveSprite(__camera)
+__camera.setFlag(SpriteFlag.Invisible, true)
+__camera.setFlag(SpriteFlag.Ghost, true)
+__camera.setFlag(SpriteFlag.StayInScreen, true)
+scene.cameraFollowSprite(__camera)
